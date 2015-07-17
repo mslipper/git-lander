8d019dc72d7ccbe4e7a6c6f804736b2dbfecbd1c
Matthew Slipper <me@matthewslipper.com>
2015-07-17 09:48:59 -0700

    WIP

--- lander-configure.js
+++ lander-configure.js
@@ -6,14 +6,24 @@ var prompts = [{
     description: 'Please enter your GitHub access token',
     name: 'token',
     required: true
+}, {
+    description: 'Please enter a GitHub repo name',
+    name: 'repoName',
+    required: true
+}, {
+    description: 'Please enter your GitHub origin name',
+    name: 'remote',
+    required: true,
+    default: 'origin'
 }];
 
 function writeConfig(res) {
-    var content = JSON.stringify(res);
+    var content = JSON.stringify(res),
+        dir = utils.getConfigDir();
 
-    fs.writeFile(utils.getHomeDir() + '/.git-lander-config.json', content,
-        utils.exitLog.bind(utils, 'Successfully wrote config file to ' +
-            '~/.git-lander-config. Please re-run this command.', 0));
+    fs.writeFile(dir + '/.git-lander-config.json', content,
+        utils.exitLog.bind(utils, 'Successfully wrote config file to ' + dir +
+            '/.git-lander-config.json. Please re-run this command.', 0));
 }
 
 prompt.start();
